601dada9a7cfc7b532952e85b3c09590b22a7491
blakebonasera <60533377+blakebonasera@users.noreply.github.com>
2020-09-17 08:53:54 -0700

    set up

--- main/models.py
+++ main/models.py
@@ -1,3 +1,45 @@
 from django.db import models
+import re
 
 # Create your models here.
+
+class UserManager(models.Manager):
+    def register_validator(self, postData):
+        errors = {}
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if len(postData['first']) < 2:
+            errors['first'] = 'You need more than 2 letters in your name'
+        if len(postData['last']) < 2:
+            errors['last'] = 'You need more than 2 letters in your name'
+        if len(postData['pw']) < 8:
+            errors['pw_length'] = "Password must be at least 8 characters"
+        if postData['pw'] != postData['cPw']:
+            errors['invalid_pw'] = 'Passwords do not match'
+        if len(postData['email']) < 8:
+            errors['email_length'] = 'Email is not long enough'
+        if not EMAIL_REGEX.match(postData['email']):                
+            errors['email'] = "Invalid email address!"
+        return errors
+    def login_validator(self, postData):
+        errors = {}
+        EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if len(postData['email']) < 8:
+            errors['email_length'] = 'Email is not long enough'
+        if not EMAIL_REGEX.match(postData['email']):                
+            errors['email'] = "Invalid email address!"
+        return errors
+
+class User(models.Model):
+    first = models.CharField(max_length=45)
+    last = models.CharField(max_length=45)
+    email = models.TextField()
+    password = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    objects = UserManager()
+
+class Book(models.Model):
+    title = models.CharField(max_length=120)
+    desc = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render , HttpResponse, redirect
+from django.contrib import messages
+from .models import User
+import bcrypt
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def register(request):
+    errors = User.objects.register_validator(request.POST)
+    if len(errors)>0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    # password = request.POST['pw']
+    pw_hash = bcrypt.hashpw(request.POST['pw'].encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    
+    new_user = User.objects.create(
+        first = request.POST['first'],
+        last= request.POST['last'],
+        email= request.POST['email'],
+        password= pw_hash
+        )
+    request.session['user_id'] = new_user.id
+    messages.success(request, 'Thats it!')
+    return redirect('/success')
+
+def login(request):
+    errors = User.objects.login_validator(request.POST)
+    if len(errors)>0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/')
+    
+    list_of_users = User.objects.filter(email=request.POST['email'])
+    if len(list_of_users) > 0:
+        user = list_of_users[0]
+        print(user)
+        if bcrypt.checkpw(request.POST['pw'].encode(), user.password.encode()):
+            request.session['user_id'] = user.id
+            return redirect('/success')
+
+def success(request):
+    if 'user_id' not in request.session:
+        return redirect('/')
+    logged_in_user = User.objects.get(id=request.session['user_id'])
+    context = {
+        'logged_in_user': logged_in_user
+    }
+    return render(request, 'success.html', context)
+
+def logout(request):
+    request.session.clear()
+    return redirect('/')
